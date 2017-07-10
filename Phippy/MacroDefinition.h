191db3530f6613648b69c8bc3f1d187f501c3e88
james <james@gmail.com>
2017-07-10 14:42:38 +0800

    init

---
+++ Phippy/MacroDefinition.h
@@ -0,0 +1,13 @@
+//
+//  MacroDefinition.h
+//  Phippy
+//
+//  Created by toby on 10/07/2017.
+//  Copyright © 2017 kg.self.edu. All rights reserved.
+//
+
+#ifndef MacroDefinition_h
+#define MacroDefinition_h
+
+
+#endif /* MacroDefinition_h */
